9c51106b1d88e5fe6679303b0774f18ee068406b
Fu Zhang <zhangfu2014@live.com>
2021-11-16 19:18:06 +0800

    implemented the get request for the transactions

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
 	"net/http"
 )
 
+type Transaction struct {
+	Id       string `json:"id" form:"id""`
+	DateInfo string `json:"data" form:"date"`
+	Amount   int    `json:"amount" form:"amount"`
+	Type     string `json:"type" form:"type"`
+	Account  string `json:"account" form:"account"`
+	Note     string `json:"note" form:"note"`
+}
+
+var db *sql.DB
+
 func main() {
+
+	initDbConnection()
 	router := gin.Default()
-	router.GET("/hi", func(c *gin.Context) {
+
+	router.GET("/transactions", func(c *gin.Context) {
+		res, _ := queryAllTransaction()
 		c.JSON(http.StatusOK, gin.H{
-			"message": "okay",
+			"transactions": res,
 		})
 	})
 
@@ -17,4 +36,38 @@ func main() {
 	if err != nil {
 		return
 	}
+}
+
+/**
+initialize the database connection
+*/
+func initDbConnection() {
+
+	var err error
+
+	db, err = sql.Open("mysql", "juizeffs:dzQbQ473f0@tcp(43.240.31.71)/juizeffs_catusMoney")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}
+
+/**
+  Function that get all entries in the transaction table
+*/
+func queryAllTransaction() (transactions []Transaction, errMsg error) {
+
+	data, err := db.Query("SELECT * FROM `transaction` WHERE 1")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for data.Next() {
+		transaction := Transaction{}
+		data.Scan(&transaction.Id, &transaction.DateInfo, &transaction.Amount, &transaction.Type, &transaction.Account, &transaction.Note)
+		fmt.Println(transaction)
+		transactions = append(transactions, transaction)
+	}
+
+	data.Close()
+	return
 }
